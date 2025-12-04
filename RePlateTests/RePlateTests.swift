ad33f195b12e45ce69b3be62e1f8b921868b294c
Basem <basemsherifhakem429@gmail.com>
2025-12-04 07:49:05 +0300

    Initial Commit

---
+++ RePlateTests/RePlateTests.swift
@@ -0,0 +1,17 @@
+//
+//  RePlateTests.swift
+//  RePlateTests
+//
+//  Created by Basem Elkhayat on 04/12/2025.
+//
+
+import Testing
+@testable import RePlate
+
+struct RePlateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
