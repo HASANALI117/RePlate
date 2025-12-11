d0b07507c02eae03c540b9a6ca619e00fe6287d7
HASANALI117 <hassanali61911@gmail.com>
2025-12-11 10:33:23 +0300

    Initial mvc-setup

---
+++ Replate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Replate
+//
+//  Created by BP-36-201-22 on 07/12/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
