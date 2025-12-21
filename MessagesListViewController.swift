098e2aac9a55963f9f4b727938294f847ea3bd2f
BP-36-201-17 <bp-36-201-17@BP-36-201-17s-iMac.local>
2025-12-21 18:44:03 +0300

    Setup Messages List view controller and storyboard changes

---
+++ MessagesListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MessagesListViewController.swift
+//  Replate
+//
+//  Created by BP-36-201-17 on 21/12/2025.
+//
+
+import UIKit
+
+class MessagesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
