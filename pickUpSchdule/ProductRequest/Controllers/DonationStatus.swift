bff364a9a59d4e0924403be73ed136976db04e0f
Mahmood2739 <202202839@student.polytechnic.bh>
2025-12-27 19:44:06 +0200

    Add files via upload

---
+++ pickUpSchdule/ProductRequest/Controllers/DonationStatus.swift
@@ -0,0 +1,55 @@
+//
+//  DonationStatus.swift
+//  ProductRequest
+//
+//  Created by mahmood on 27/12/2025.
+//
+
+import UIKit
+
+class DonationStatus: UIViewController {
+
+    @IBOutlet weak var table: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        table.dataSource = self
+        table.delegate = self
+        table.register(UINib(nibName: "StatusCell", bundle: nil), forCellReuseIdentifier: "StatusCell")
+        table.showsHorizontalScrollIndicator = false
+        table.showsVerticalScrollIndicator = false
+        
+    }
+
+    
+
+    @IBAction func clos(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+    
+}
+extension DonationStatus: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "StatusCell") as? StatusCell else {
+            return UITableViewCell()
+        }
+
+
+        return cell
+    }
+
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+    }
+
+    
+}
---
+++ pickUpSchdule/ProductRequest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProductRequest
+//
+//  Created by mahmood on 23/12/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
